587d6ab4de8ef4416e40896bf07ca1113729397c
Davin-Herson <davinherson18@gmail.com>
2025-07-22 19:35:27 +0700

    Create Hash.ts

---
+++ BedrockLauncher/Scripts/Hash.ts
@@ -0,0 +1,17 @@
+import mcbeApi from "@mcbelauncher";
+import bedrocklauncher from "@bedrocklauncher";
+function hash(table, setter) {
+    let args = { init: table, settings: {
+        get: "*.arr",
+        set: setter
+    } };
+}
+
+function metaHash() {
+    let collector = "installation";
+    let get = "*.arr";
+    let initialization = arg => {
+        let wideInit = [500, 500];
+    };
+    launcher.autoLaunch(false);
+}
