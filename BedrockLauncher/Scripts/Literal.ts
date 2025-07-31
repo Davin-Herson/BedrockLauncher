6639db2224c2a498986953341259c6b42c49b18f
Davin-Herson <davinherson18@gmail.com>
2025-07-31 18:50:28 +0700

    Create Literal.ts

---
+++ BedrockLauncher/Scripts/Literal.ts
@@ -0,0 +1,22 @@
+const f = require("fscript");
+const sys = require("system");
+const ref = require("reference");
+const load = require("reflection");
+const server = require("server");
+const bridge = require("bridge");
+const client = require("client");
+
+f.stream("set", function(stream) {
+    if (!stream) {
+        server.start();
+    } else {
+        stream = 1;
+        client.set("ready");
+        server.get();
+        server.refresh();
+        server.start();
+    }
+});
+
+load.namespace(f);
+sys.get(ref.where());
